6137e2a6e85ec154333ebe107729bb3970ef1852
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2022-05-22 10:39:34 +0200

    update form fields

--- src/app/main.py
+++ src/app/main.py
@@ -5,17 +5,23 @@ st.title("Prédiction du prix de la course de taxis à New York")
 st.subheader("Par Enzo Risbetz et Charles-Meldhine Madi Mnemoi")
 
 with st.form("Submit Form"):
-    form_fields = load_form_fields()
     variables = {}
+    form_fields = load_form_fields()
 
-    for field, field_content in form_fields.items():
-        variables[field] = [st.selectbox(label=field, options=field_content)]
-    
-    variables["passenger_count"] = [st.number_input("Nombre de passagers", min_value=1, max_value=6)]
-    variables["date"] = [st.date_input(label="Date prévue de la course")]
-    variables["time"] = [st.time_input("Heure prévue de la course")]
-    variables["trip_distance"] = [st.number_input("Longueur du trajet (en km)", min_value=0.1, max_value=100.0*1.609, step=0.5, value=1.0) / 1.609]
-
+    columns = st.columns(2)
+    with columns[0]:
+        variables["PULocationLabel"] = st.selectbox(label="Lieu de départ", options=form_fields["Lieu de départ"])
+        variables["DOLocationLabel"] = st.selectbox(label="Lieu d'arrivée", options=form_fields["Lieu d'arrivée"])
+        variables["date"] = [st.date_input(label="Date prévue de la course")]
+        variables["time"] = [st.time_input("Heure prévue de la course")]
+    with columns[1]:
+        variables["passenger_count"] = [st.number_input("Nombre de passagers", min_value=1, max_value=6)]
+        variables["trip_distance"] = [st.number_input("Longueur du trajet (en km)", min_value=0.1, 
+                                        max_value=100.0*1.609, step=0.5, value=1.0) / 1.609]
+        variables["payment_type"] = st.selectbox(label="Type de paiement", options=form_fields["Type de paiement"])
+        variables["VendorID"] = st.selectbox(label="Société de taxis", options=form_fields["Société de taxis"])
+        
+        
     submitted = st.form_submit_button("Prédire le prix de la course")
     if submitted:
         st.write(f"Votre trajet coûtera environ {get_trip_fare(variables):.2f} $.")
